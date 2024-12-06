8488e87eaddb355cdaa22189d64fe6cb55c80fe2
verdanix <business.prismoid@gmail.com>
2024-12-05 18:27:37 -0800

    Updated javadoc comments
    - Replaced the dashes with unordered HTML lists
    
    - Added package-info.java

--- core/src/main/java/dev/masonroot/nora/exceptions/ConfigManagerException.java
+++ core/src/main/java/dev/masonroot/nora/exceptions/ConfigManagerException.java
@@ -9,13 +9,19 @@ import lombok.NonNull;
  * with a message and an optional cause. It is used to indicate issues related to configuration
  * management within the application.
  *
- * <p><b>Thread Safety:</b> <br>
- * This class is thread-safe. It does not maintain any state and only provides constructors for
- * creating exception instances.
+ * <p><b>Thread Safety:</b>
  *
- * <p><b>Notes:</b> <br>
- * - This class can be used to wrap other exceptions that occur during configuration management. -
- * Ensure to provide meaningful messages and causes when creating instances of this exception.
+ * <ul>
+ *   <li>This class is thread-safe. It does not maintain any state and only provides constructors
+ *       for creating exception instances.
+ * </ul>
+ *
+ * <p><b>Notes:</b>
+ *
+ * <ul>
+ *   <li>This class can be used to wrap other exceptions that occur during configuration management.
+ *   <li>Ensure to provide meaningful messages and causes when creating instances of this exception.
+ * </ul>
  */
 public class ConfigManagerException extends RuntimeException {
 
@@ -26,14 +32,20 @@ public class ConfigManagerException extends RuntimeException {
    * describes the error. It is useful for indicating issues related to configuration management
    * within the application.
    *
-   * <p><b>Why:</b> <br>
-   * Throwing custom exceptions with meaningful messages helps in identifying and diagnosing issues
-   * specific to configuration management. It provides a way to capture and handle errors that occur
-   * during configuration operations.
+   * <p><b>Why:</b>
    *
-   * <p><b>Notes:</b> <br>
-   * - Ensure to provide a meaningful message when creating instances of this exception. - The
-   * {@code @NonNull} annotation indicates that the parameter should not be null.
+   * <ul>
+   *   <li>Throwing custom exceptions with meaningful messages helps in identifying and diagnosing
+   *       issues specific to configuration management. It provides a way to capture and handle
+   *       errors that occur during configuration operations.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>Ensure to provide a meaningful message when creating instances of this exception.
+   *   <li>The {@code @NonNull} annotation indicates that the parameter should not be null.
+   * </ul>
    *
    * @param message the detail message; must not be null.
    * @throws NullPointerException if the message is null.
@@ -50,15 +62,22 @@ public class ConfigManagerException extends RuntimeException {
    * configuration management within the application and wrapping other exceptions that occur during
    * configuration operations.
    *
-   * <p><b>Why:</b> <br>
-   * Throwing custom exceptions with meaningful messages and causes helps in identifying and
-   * diagnosing issues specific to configuration management. It provides a way to capture and handle
-   * errors that occur during configuration operations and wrap other exceptions that may have
-   * caused the error.
+   * <p><b>Why:</b>
    *
-   * <p><b>Notes:</b> <br>
-   * - Ensure to provide a meaningful message and cause when creating instances of this exception. -
-   * The {@code @NonNull} annotation indicates that the parameters should not be null.
+   * <ul>
+   *   <li>Throwing custom exceptions with meaningful messages and causes helps in identifying and
+   *       diagnosing issues specific to configuration management. It provides a way to capture and
+   *       handle errors that occur during configuration operations and wrap other exceptions that
+   *       may have caused the error.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>Ensure to provide a meaningful message and cause when creating instances of this
+   *       exception.
+   *   <li>The {@code @NonNull} annotation indicates that the parameters should not be null.
+   * </ul>
    *
    * @param message the detail message; must not be null.
    * @param cause the cause of the exception; must not be null.
