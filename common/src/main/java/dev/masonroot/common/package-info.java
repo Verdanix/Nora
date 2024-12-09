e67e2e89c1bacd16ceec7621f5f12ce7c27b3063
verdanix <business.prismoid@gmail.com>
2024-12-08 19:49:09 -0800

    Added a common module
    - Moved NoraLogger to the common module
    
    - Moved the logback.xml to common module

---
+++ common/src/main/java/dev/masonroot/common/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains utility classes and interfaces for common functionalities used across the
+ * Nora application.
+ *
+ * <p><b>Why:</b>
+ *
+ * <ul>
+ *   <li>To provide reusable utility methods and classes that can be used by different modules.
+ *   <li>To reduce code duplication by centralizing common functionalities.
+ * </ul>
+ */
+package dev.masonroot.common;
