619171194535229d737c2e37bd1a8c7e531787fa
Mason Root <contact@masonroot.dev>
2025-01-10 21:03:11 -0800

    Added/Updated javadoc comments and created exceptions
    Added
    - Exceptions package with SttEngineInitializationException
    - VoskInitializationException

---
+++ audio/src/main/java/dev/masonroot/audio/exceptions/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains custom exception classes for the audio module.
+ *
+ * <p><b>Why:</b>
+ *
+ * <ul>
+ *   <li>To handle specific error conditions related to audio processing.
+ *   <li>To provide meaningful error messages and facilitate debugging.
+ * </ul>
+ */
+package dev.masonroot.audio.exceptions;
