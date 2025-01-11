0563e376a0e8c3b8e9b3a4f984c32f6d81d330f0
Mason Root <contact@masonroot.dev>
2025-01-10 20:54:53 -0800

    Added/Updated Javadoc comments
    Added
    - Added Javadoc comments to VoskSttEngine and SttEngine
    - Synchronized keyword to VoskSttEngine transcribe method
    - Punctuation

--- audio/src/main/java/dev/masonroot/audio/stt/SttEngine.java
+++ audio/src/main/java/dev/masonroot/audio/stt/SttEngine.java
@@ -4,8 +4,63 @@ import dev.masonroot.audio.AudioInterface;
 import java.nio.file.Path;
 import lombok.NonNull;
 
-public interface SttEngine {
+/**
+ * Speech-to-Text (STT) engine interface for converting spoken language into written text.
+ *
+ * <p>This interface defines the methods required for initializing an STT engine, transcribing audio
+ * data, and managing resources. Implementations of this interface should provide the necessary
+ * functionality to perform speech recognition tasks.
+ *
+ * <p><b>Thread Safety:</b>
+ *
+ * <ul>
+ *   <li>Implementations should ensure thread safety when accessing shared resources.
+ * </ul>
+ *
+ * <p><b>Notes:</b>
+ *
+ * <ul>
+ *   <li>Implementations should handle any necessary audio processing and transcription logic.
+ * </ul>
+ */
+public interface SttEngine extends AutoCloseable {
+  /**
+   * Initializes the STT engine with the specified audio interface and model path.
+   *
+   * <p><b>Why:</b>
+   *
+   * <ul>
+   *   <li>To set up the necessary resources for speech-to-text conversion.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>The {@code @NonNull} annotation indicates that the parameters should not be null.
+   * </ul>
+   *
+   * @param audio the {@code AudioInterface} for handling audio input; must not be null
+   * @param modelPath the path to the STT model; must not be null.
+   */
   void initialize(@NonNull final AudioInterface audio, final Path modelPath);
 
+  /**
+   * Transcribes audio data to text.
+   *
+   * <p><b>Why:</b>
+   *
+   * <ul>
+   *   <li>To convert spoken language into written text.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>The method should handle any necessary audio processing and transcription logic.
+   * </ul>
+   *
+   * @param timeoutInMs the timeout in milliseconds for the transcription process.
+   * @return the transcribed text.
+   */
   String transcribe(final long timeoutInMs);
 }
--- audio/src/main/java/dev/masonroot/audio/stt/VoskSttEngine.java
+++ audio/src/main/java/dev/masonroot/audio/stt/VoskSttEngine.java
@@ -10,27 +10,84 @@ import org.vosk.LogLevel;
 import org.vosk.Model;
 import org.vosk.Recognizer;
 
-public class VoskSttEngine implements SttEngine, AutoCloseable {
+/**
+ * Vosk STT engine for converting spoken language into written text using the Vosk speech
+ * recognition library.
+ *
+ * <p>This class provides methods to initialize the STT engine, transcribe audio data, and manage
+ * resources. It uses the Vosk library to perform speech recognition tasks.
+ *
+ * <p><b>Thread Safety:</b>
+ *
+ * <ul>
+ *   <li>This class is thread-safe. The read and write methods are synchronized to prevent
+ *       concurrent access.
+ * </ul>
+ *
+ * <p><b>Notes:</b>
+ *
+ * <ul>
+ *   <li>This class implements {@code AutoCloseable} to ensure that audio resources are properly
+ *       released.
+ *   <li>Ensure that the microphone and speaker lines are properly initialized before using this
+ *       class.
+ * </ul>
+ */
+public class VoskSttEngine implements SttEngine {
   private AudioInterface audioInterface;
-  private Path modelPath;
   private Model model;
   private Recognizer recognizer;
 
+  /**
+   * Initializes the STT engine with the specified audio interface and model path.
+   *
+   * <p><b>Why:</b>
+   *
+   * <ul>
+   *   <li>To set up the necessary resources for speech-to-text conversion.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>The {@code @NonNull} annotation indicates that the parameters should not be null.
+   * </ul>
+   *
+   * @param audio the {@code AudioInterface} for handling audio input; must not be null.
+   * @param modelPath the path to the STT model; must not be null.
+   */
   @Override
   public void initialize(@NonNull final AudioInterface audio, @NonNull final Path modelPath) {
     LibVosk.setLogLevel(LogLevel.INFO);
     this.audioInterface = audio;
-    this.modelPath = modelPath;
     try {
-      this.model = new Model(this.modelPath.toString());
+      this.model = new Model(modelPath.toString());
       this.recognizer = new Recognizer(this.model, audio.microphone().getFormat().getSampleRate());
     } catch (IOException e) {
       NoraLogger.trace("Failed to load Vosk model.", e);
     }
   }
 
+  /**
+   * Transcribes audio data to text.
+   *
+   * <p><b>Why:</b>
+   *
+   * <ul>
+   *   <li>To convert spoken language into written text.
+   * </ul>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>The method should handle any necessary audio processing and transcription logic.
+   * </ul>
+   *
+   * @param timeoutInMs the timeout in milliseconds for reading audio data.
+   * @return the transcribed text.
+   */
   @Override
-  public String transcribe(long timeoutInMs) {
+  public synchronized String transcribe(long timeoutInMs) {
     NoraLogger.info("Transcribing audio...");
     byte[] bytes = this.audioInterface.read(timeoutInMs);
     this.recognizer.acceptWaveForm(bytes, bytes.length);
